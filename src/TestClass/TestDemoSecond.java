6a1e1da2fa4624aeb5676d33665cf9950578205c
RijalP <LajirMac@lajirs-mbp.home>
2019-05-10 13:44:09 -0400

    second test calss added

---
+++ src/TestClass/TestDemoSecond.java
@@ -0,0 +1,12 @@
+package TestClass;
+
+import org.testng.annotations.Test;
+
+public class TestDemoSecond {
+	
+	@Test
+	public void test(){
+		System.out.println("this is testdemosecond");
+	}
+
+}
